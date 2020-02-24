7ef71b0849942ff7e88426b74c07ed1953ee401c
Alisson Oliveira <alissonoliveiram@gmail.com>
2020-02-23 22:15:32 -0300

    Select inputs

--- web/src/components/dashboard/index.js
+++ web/src/components/dashboard/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import './style.css';
+
 function Dashboard({ onSubmit }) {
     const [ options, setOptions ] = useState([]);
 
@@ -21,30 +23,44 @@ function Dashboard({ onSubmit }) {
 
     function showInput() {
         let optionSelect = document.querySelector('select#optionsForm').value;
+        document.querySelector('div.form').style.display = "block";
+        document.querySelector('form.login-form').style.display = "grid";
         switch(optionSelect) {
             case 'password':
                 if(!options.includes('password')) {
                     setOptions([...options, 'password']);
+                    document.querySelector('input.password').style.display = "block";
+                    document.querySelector('button.password').style.display = "block";
                 }
                 break;
             case 'name':
                 if(!options.includes('name')) {
                     setOptions([...options, 'name']);
+                    document.querySelector('input.name').style.display = "block";
+                    document.querySelector('button.name').style.display = "block";
                 }
                 break;
             case 'birthday':
                 if(!options.includes('birthday')) {
                     setOptions([...options, 'birthday']);
+                    document.querySelector('input.birthday').style.display = "block";
+                    document.querySelector('button.birthday').style.display = "block";
                 }
                 break;
             case 'cpf':
                 if(!options.includes('cpf')) {
                     setOptions([...options, 'cpf']);
+                    document.querySelector('input.cpf').style.display = "block";
+                    document.querySelector('button.cpf').style.display = "block";
                 }
                 break;
             default:
                 console.log('Error');
         }
+    }
+
+    function eraseInput(classInput) {
+
     }
 
     return(
@@ -61,11 +77,18 @@ function Dashboard({ onSubmit }) {
 
             <div className="form">
                 <form className="login-form">
-                <input type="password" required placeholder="Password" className="inputs password"/>
-                <input type="text" required placeholder="Name" className="inputs name"/>
-                <input type="date" required placeholder="Date of birthday" className="inputs birthday"/>
-                <input type="text" required placeholder="CPF" className="inputs cpf"/>
-                <button type="submit">Update info</button>
+                    <input type="password" required placeholder="Password" className="inputs password"/>
+                    <button onClick={() => eraseInput("password")} className="b password">X</button>
+
+                    <input type="text" required placeholder="Name" className="inputs name"/>
+                    <button onClick={() => eraseInput("name")} className="b name">X</button>
+
+                    <input type="date" required placeholder="Date of birthday" className="inputs birthday"/>
+                    <button onClick={() => eraseInput("birthday")} className="b birthday">X</button>
+
+                    <input type="text" required placeholder="CPF" className="inputs cpf"/>
+                    <button onClick={() => eraseInput("cpf")} className="b cpf">X</button>
+                    <button type="submit">Update info</button>
                 </form>
             </div>
         </div>
